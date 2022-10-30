d1825138efb21cad96232b1026f299b6ad71f81c
LuizHenrique <luizhenrique9x08@gmail.com>
2022-10-30 14:23:49 -0300

    Sistema de Login Adicionado

---
+++ js/dados-login.js
@@ -0,0 +1,6 @@
+// Gmail do Usuário
+
+
+let info_gmail_user = document.querySelector('p.info_gmail_user')
+let gmailConta = localStorage.getItem('gmail')
+info_gmail_user.innerHTML = gmailConta
--- js/login-verificar.js
+++ js/login-verificar.js
@@ -1,32 +1,42 @@
-let nome = document.getElementById('usuario')
 let gmail = document.querySelector('input#gmail')
 let senha = document.querySelector('input#senha')
+let loginNone = document.querySelector('a#login_none_cod1')
+let verificar_gmail = localStorage.getItem('gmail')
+let verificar_senha = localStorage.getItem('senha')
+let verificar_login = localStorage.getItem('verificar_login')
+let icone_login = document.querySelector('a.icone_login')
 
 function verificarLogin() {
-    let nome_format = nome.value
     let gmail_format = gmail.value
     let senha_format = senha.value
     //Variaveis abaixo para confirmar o login
-    let confirm_nome = localStorage.getItem('nome')
-    let confirm_gmail = localStorage.getItem('gmail')
-    let confirm_senha = localStorage.getItem('senha')
-    if(nome_format != '' && gmail_format != '' && senha_format != ''){
-        if(
-            (confirm_nome == '' || confirm_nome != nome_format)
-            && (confirm_gmail == '' || confirm_gmail != gmail_format)
-            && (confirm_senha == '' || confirm_senha != senha_format)
-            ){
-                localStorage.setItem('nome', nome_format)
-                localStorage.setItem('gmail', gmail_format)
-                localStorage.setItem('senha', senha_format)
-                alert('Logado com sucesso!')
-                window.location.href = '../index.html'
+    if(
+        (gmail_format != '' && gmail_format != verificar_gmail) && (senha_format != verificar_senha && senha_format != '')){
+        alert('Logado com sucesso!')
+        window.location.href = '../index.html'//Vai retornar a página inicial
+        localStorage.setItem('gmail', gmail_format)
+        localStorage.setItem('senha', senha_format)
+        localStorage.setItem('verificar_login', 'ContaCriada')
 
-        }else{
-            alert('Nome já existente!')
-        }
-    }else{
-        alert('Campo vazio!')
+        loginNone.style.display = 'none'
+        icone_login.style.display = 'flex'
+    }
+    else if(gmail_format == '' && senha_format == ''){
+        alert('Campo Vazio')
+    }
+    else if(gmail_format == '' || senha_format == ''){
+        alert('Campo Vazio')
+    }
+    else {
+        alert('Conta já existente')
     }
 }
 
+if(verificar_login == 'ContaCriada'){
+    icone_login.style.display = 'flex'
+    loginNone.style.display = 'none'
+}
+
+function sairConta() {
+    localStorage.removeItem('verificar_login')
+}
